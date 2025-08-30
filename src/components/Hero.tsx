ae0334976430e9c58470abc13f1738a3360b6992
priyesshrai <priyeshrai369@gmail.com>
2025-08-30 16:37:21 +0530

    style: adjust heading styles for improved readability in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -36,7 +36,7 @@ export default function Hero() {
             <Wrapper>
                 <div className='w-full relative grid md:grid-cols-2 grid-cols-1 md:gap-0 gap-5'>
                     <div className="w-full h-full relative flex flex-col justify-center md:items-start items-center">
-                        <h1 className='font-extrabold lg:text-5xl md:text-4xl text-2xl text-primary-dark md:text-left text-center'>
+                        <h1 className='font-extrabold lg:text-5xl md:text-4xl text-[28px] text-primary-dark md:text-left text-center md:mt-0 mt-5 leading-[1.3]'>
                             Because Every Patient Deserves <span className='!font-inter text-secondry'>Safer Care</span>
                         </h1>
                         <Subheading classname='md:!text-xl md:text-left !mt-2 text-center max-w-lg'>
