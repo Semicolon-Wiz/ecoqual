6ebefcea6a7c4720e1652330e42aab2311d2f212
priyesshrai <priyeshrai369@gmail.com>
2025-09-06 15:51:21 +0530

    Add SVG icons and WhereToUse component for Wet Wash Gloves section
    - Introduced two new SVG icons: icon-9.svg and icon-11.svg for use in the application.
    - Created a new WhereToUse component to display usage scenarios for Wet Wash Gloves.
    - Updated the WetWashGloves page to include the WhereToUse component, enhancing the user experience with informative content.

--- src/app/wet-wash-gloves/page.tsx
+++ src/app/wet-wash-gloves/page.tsx
@@ -1,6 +1,7 @@
 import AboutWet from "./AboutWet";
 import Details from "./Details";
 import WetHero from "./WetHero";
+import WhereToUse from "./WhereToUse";
 
 
 export default function WetWashGloves() {
@@ -8,7 +9,8 @@ export default function WetWashGloves() {
         <main className="relative w-full">
             <WetHero />
             <AboutWet />
-            <Details/>
+            <Details />
+            <WhereToUse />
         </main>
     )
 }
