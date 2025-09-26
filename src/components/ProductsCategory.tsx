5f4d2168a1de8e75166405d3cb81b39433823002
priyesshrai <priyeshrai369@gmail.com>
2025-09-26 15:44:21 +0530

    update category

--- src/components/ProductsCategory.tsx
+++ src/components/ProductsCategory.tsx
@@ -1,43 +1,40 @@
+'use client';
 import { Heading, Section, Subheading, Wrapper } from '@/utils/Section'
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import { MoveUpRight } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
 
-interface Category {
-    name: string;
+interface Data {
+    id: string;
+    title: string;
+    slug: string;
+    category_heading: string;
     description: string;
     image: string;
-    slug: string;
 }
 
+interface ApiResponse<T> {
+    success: boolean;
+    message: string;
+    data: T;
+}
+
+const fetchCategories = async (): Promise<Data[]> => {
+    const res = await axios.get<ApiResponse<Data[]>>(
+        "https://inforbit.in/demo/ecoqual/api/menu"
+    );
+    return res.data.data;
+};
+
 export default function ProductsCategory() {
-    const category: Category[] = [
-        {
-            name: 'House Keeping Products',
-            description: 'From floor cleaners to mops, brooms, and dusters, our housekeeping range ensures spotless hygiene across hospitals, institutions, and corporate spaces. These products are designed to remove tough stains, control odors, and maintain a clean, safe, and fresh environment every day.',
-            image: '/images/category/house-keeping/house-keeping.svg',
-            slug: 'house-keeping'
-        },
-        {
-            name: 'Surgical Products',
-            description: 'Our surgical essentials include caps, gowns, and protective wear made from high-quality, breathable fabrics. Designed for safety, comfort, and infection control, these products support healthcare professionals during surgeries, procedures, and patient care.',
-            image: '/images/category/surgical-products/surgical-products.svg',
-            slug: 'surgical'
-        },
-        {
-            name: 'Institutional Products',
-            description: 'Durable and comfortable uniforms including scrub suits, aprons, lab coats, and patient gowns. Tailored for a professional look and easy wear, they are suitable for doctors, nurses, staff, and patients in hospitals and institutions.',
-            image: '/images/category/institutional/institutional-products.svg',
-            slug: 'institutional'
-        },
-        {
-            name: 'Kits',
-            description: 'Comprehensive kits like Viral Barrier / HIV Premium Kits offer all-in-one protection with gowns, gloves, masks, and more. Ready-to-use and easy to carry, they ensure safety, hygiene, and convenience in critical care and emergency situations.',
-            image: '/images/category/kits/kits.svg',
-            slug: 'kits'
-        }
-    ]
+    const { data, error, isLoading } = useQuery<Data[], Error>({
+        queryKey: ["categories", 'home'],
+        queryFn: fetchCategories,
+        staleTime: 1000 * 60 * 5,
+    });
     return (
         <Section>
             <Wrapper>
@@ -53,16 +50,16 @@ export default function ProductsCategory() {
 
                     <div className='w-full relative grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5'>
                         {
-                            category.map((items, idx) => (
+                            data?.map((items, idx) => (
                                 <div key={idx} className='w-full h-full border border-gray-200 md:rounded-2xl rounded-lg md:p-5 p-2.5 '>
                                     <div className='relative w-full'>
-                                        <Image src={items.image} alt={items.name} width={500} height={400}
+                                        <Image src={items.image} alt={items.title} width={500} height={400}
                                             className='w-full h-[200px] object-cover object-center rounded-lg'
                                         />
                                     </div>
                                     <div className='mt-5 relative'>
                                         <h3 className='font-semibold !font-montserrat lg:text-2xl md:text-lg text-base text-zinc-700'>
-                                            {items.name}
+                                            {items.title}
                                         </h3>
                                         <Subheading classname='text-left'>
                                             {items.description}
