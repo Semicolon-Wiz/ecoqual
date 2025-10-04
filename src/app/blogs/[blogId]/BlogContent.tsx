2d5a4b68039954ea6cd541f632b9d456e0d3b245
priyesshrai <priyeshrai369@gmail.com>
2025-10-04 12:32:06 +0530

    Update blog page

--- src/app/blogs/[blogId]/BlogContent.tsx
+++ src/app/blogs/[blogId]/BlogContent.tsx
@@ -3,13 +3,14 @@
 import { BlogSkeleton } from "@/components/Skeleton";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import Image from "next/image";
 
 export interface Blog {
     id: number;
     title: string;
     slug: string;
     blog_image: string;
-    bog_description: string;
+    description: string;
     paragraphs: string[]
 }
 export interface BlogApi {
@@ -38,8 +39,14 @@ export default function BlogContent({ blogId }: { blogId: string }) {
     }
 
     return (
-        <div className='relative w-full'>
-
-        </div>
+        <>
+            <h1 className='font-bold lg:text-4xl md:text-2xl text-xl text-primary leading-[1.3] '>
+                {data?.title}
+            </h1>
+            <div className="mt-3 lg:text-base text-sm text-zinc-600 font-medium"
+                dangerouslySetInnerHTML={{ __html: data?.description! }}
+            />
+            <Image src={data?.blog_image!} alt={data?.title!} width={800} height={500} className='w-full h-[400px] object-cover mt-5 rounded-lg' />
+        </>
     )
 }
