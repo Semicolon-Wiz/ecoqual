dd8e120530f285fe5c84e819e1386dbb06afb78e
priyesshrai <priyeshrai369@gmail.com>
2025-10-08 14:18:12 +0530

    update hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,16 @@
+'use client';
 import { ButtonPrimary, ButtonSecondry, Section, Subheading, Wrapper } from '@/utils/Section'
 import Image from 'next/image'
-import React from 'react'
+import React, { useRef } from 'react'
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+    type CarouselApi
+} from "@/components/ui/carousel"
+import Autoplay from "embla-carousel-autoplay";
 
 interface Data {
     title: string;
@@ -9,76 +19,47 @@ interface Data {
 }
 
 export default function Hero() {
-    // const data: Data[] = [
-    //     {
-    //         title: 'Housekeeping & Cleaning Supplies',
-    //         summary: 'Eco-friendly sanitizers, phenyls, disinfectants, and multi-purpose cleaning agents ensure spotless hygiene across healthcare and corporate facilities.',
-    //         image: '/images/hero/icon-01.svg'
-    //     },
-    //     {
-    //         title: 'Hospital Consumables',
-    //         summary: 'From sterile HIV kits to durable gowns, aprons, gloves, and masks, our consumables deliver safety and protection in every setting.',
-    //         image: '/images/hero/icon-02.svg'
-    //     },
-    //     {
-    //         title: 'Patient Care Essentials',
-    //         summary: 'Products designed to support comfort and infection control across wards, ICUs, and operation theatres, ensuring better patient well-being.',
-    //         image: '/images/hero/icon-03.svg'
-    //     },
-    //     {
-    //         title: 'Wet Wash Gloves',
-    //         summary: 'A modern hygiene solution that reduces infection risks, enhances patient comfort, and saves caregiver time with safe, disposable, and convenient use.',
-    //         image: '/images/hero/icon-04.svg'
-    //     },
-    // ]
+    const banner = [
+        '/images/hero/banner-01.webp',
+        '/images/hero/banner-02.webp',
+        '/images/hero/banner-03.webp',
+    ]
+    const [api, setApi] = React.useState<CarouselApi>()
+    const plugin = useRef(Autoplay({ delay: 4000, stopOnInteraction: false, stopOnMouseEnter: true }))
     return (
-        <Section classname=''>
-            <Wrapper>
-                <div className='w-full relative grid md:grid-cols-2 grid-cols-1 md:gap-0 gap-5'>
-                    <div className="w-full h-full relative flex flex-col justify-center md:items-start items-center">
-                        <h1 className='font-extrabold lg:text-5xl md:text-4xl text-[28px] text-primary md:text-left text-center md:mt-0 mt-5 leading-[1.3]'>
-                            Ecoqual Sets the Standard in <span className='!font-inter text-secondry'>Hygiene Care</span>
-                        </h1>
-                        <Subheading classname='md:!text-xl md:text-left !mt-2 text-center max-w-xl'>
-                            From cleaning supplies to medical essentials, Ecoqual delivers safer and more reliable solutions for healthcare.
-                        </Subheading>
-                        <div className='relative mt-10 flex items-center gap-2 md:justify-start justify-center'>
-                            <ButtonPrimary>
-                                Contact Us
-                            </ButtonPrimary>
-                            <ButtonSecondry>
-                                Request Demo
-                            </ButtonSecondry>
-                        </div>
-                    </div>
+        <Carousel className="w-full z-10"
+            plugins={[plugin.current]}
+            opts={{
+                loop: true,
+            }}
+            onMouseEnter={plugin.current.stop}
+            onMouseLeave={plugin.current.reset}
+            setApi={setApi}
+        >
+            <CarouselContent>
+                {
+                    banner.map((data, key) => (
+                        <CarouselItem key={key}>
+                            <Image src={data} alt='Bannet' width={1200} height={500} className='w-full h-auto' />
+                        </CarouselItem>
+                    ))
+                }
+            </CarouselContent>
+            <CarouselPrevious
+                onClick={() => {
+                    api?.scrollPrev();
+                    plugin.current?.reset();
+                }}
+                className='border-none w-11 h-11 bg-zinc-900 text-white left-10 text-3xl cursor-pointer hover:bg-zinc-800'
+            />
+            <CarouselNext
+                onClick={() => {
+                    api?.scrollNext();
+                    plugin.current?.reset();
+                }}
+                className='border-none w-11 h-11 bg-zinc-900 text-white text-3xl right-10 cursor-pointer hover:bg-zinc-800'
+            />
+        </Carousel>
 
-                    <div className="w-full h-full relative flex items-center md:justify-end justify-center">
-                        <Image src={'/images/hero/hero-main.png'} width={800} height={450}
-                            alt='Hero Image'
-                            className='w-full h-auto max-w-[570px] ' />
-                    </div>
-                </div>
-
-                {/* <div className='relative w-full mt-14 grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-5'>
-                    {
-                        data.map((items, idx) => (
-                            <div className='w-full h-full flex gap-2 items-start' key={idx}>
-                                <div className='w-8 h-8 shrink-0'>
-                                    <Image src={items.image} alt={items.title} width={32} height={32} className='w-8 h-8' />
-                                </div>
-                                <div className='flex-1 flex flex-col gap-1'>
-                                    <h3 className='font-medium text-lg text-primary-dark leading-[1.2]'>
-                                        {items.title}
-                                    </h3>
-                                    <Subheading classname='!text-left leading-[1.2]'>
-                                        {items.summary}
-                                    </Subheading>
-                                </div>
-                            </div>
-                        ))
-                    }
-                </div> */}
-            </Wrapper>
-        </Section>
     )
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -143,105 +143,107 @@ export default function NavBar() {
 
 
     return (
-        <header className='relative w-full'>
-            <nav className='relative w-full max-w-7xl mx-auto px-5 py-2 flex items-center justify-between'>
-                <div className='relative shrink-0 w-max'>
-                    <Link href={'/'}>
-                        <Image src={'/images/logo/logo.svg'} width={1000} height={600} alt='Ecoqual Healthcare Solutions' className='w-28 h-auto ' />
-                    </Link>
-                </div>
+        <>
+            <header className='sticky top-0 bg-white/10 backdrop-blur-2xl z-20 w-full'>
+                <nav className='relative w-full max-w-7xl mx-auto px-5 py-2 flex items-center justify-between'>
+                    <div className='relative shrink-0 w-max'>
+                        <Link href={'/'}>
+                            <Image src={'/images/logo/logo.svg'} width={1000} height={600} alt='Ecoqual Healthcare Solutions' className='w-28 h-auto ' />
+                        </Link>
+                    </div>
 
-                <div className='lg:flex hidden relative w-max items-center gap-2'>
-                    {
-                        menuItems.map(items => (
-                            <Fragment key={items.key} >
-                                <div className="relative group/menu">
-                                    {
-                                        items.key !== 'products' && items.key !== 'wet-wash-gloves' ? (
-                                            <Link href={items.path} className={`relative text-lg transition-all duration-200 ease-linear px-3.5 py-1.5 ${currentPath === items.path ? ' bg-primary text-white rounded-full' : ' text-zinc-800'} `}>
-                                                {items.name}
-                                            </Link>
-
-                                        ) : (
-                                            <button type="button"
-                                                className={`transition-all duration-200 ease-linear !font-inter cursor-pointer inline-flex items-center gap-1  text-lg font-normal`}
-                                            >
-                                                {items.name}
-                                                <ChevronDown className="transition-transform duration-200 ease-linear group-hover/menu:rotate-180" />
-                                            </button>
-                                        )}
-                                    {items.key === 'wet-wash-gloves' ? (
-                                        <div
-                                            className="absolute top-full left-1/2 -translate-x-1/2 mt-2 z-50 w-max bg-primary rounded-xl shadow-md p-3 opacity-0 invisible translate-y-2 transition-all duration-200 group-hover/menu:opacity-100 group-hover/menu:visible group-hover/menu:translate-y-0 "
-                                        >
-                                            {items.submenu?.map((submenu) => (
-                                                <Link
-                                                    key={submenu.key}
-                                                    href={submenu.path}
-                                                    className="group/item text-lg flex items-center gap-2.5 px-3 py-1.5 rounded text-white hover:bg-warm hover:text-neutral-100 transition-colors duration-200"
+                    <div className='lg:flex hidden relative w-max items-center gap-2'>
+                        {
+                            menuItems.map(items => (
+                                <Fragment key={items.key} >
+                                    <div className="relative group/menu">
+                                        {
+                                            items.submenu!?.length > 0 ? (
+                                                <button type="button"
+                                                    className={`${console.log(currentPath)} transition-all duration-200 ease-linear !font-inter cursor-pointer inline-flex items-center gap-1  text-lg font-normal`}
                                                 >
-                                                    <MoveUpRight
-                                                        className="w-4 shrink-0 transition-transform duration-200 group-hover/item:-translate-y-0.5 group-hover/item:translate-x-0.5 group-hover/item:text-white"
-                                                    />
-                                                    <span className="truncate">{submenu.name}</span>
-                                                </Link>
-                                            ))}
-                                        </div>
-                                    ) : items.key === 'products' ? (
-                                        <div
-                                            className="absolute top-full left-1/2 -translate-x-1/2 mt-2 z-50 w-max bg-primary rounded-xl shadow-md p-3 opacity-0 invisible translate-y-2 transition-all duration-200 group-hover/menu:opacity-100 group-hover/menu:visible group-hover/menu:translate-y-0 "
-                                        >
-                                            {isLoading ? (
-                                                <span className="text-white px-3 py-1.5">Loading...</span>
+                                                    {items.name}
+                                                    <ChevronDown className="transition-transform duration-200 ease-linear group-hover/menu:rotate-180" />
+                                                </button>
                                             ) : (
-
-                                                <>
-                                                    {data?.map((submenu) => (
-                                                        <Link
-                                                            key={submenu.title}
-                                                            href={`/product/${submenu.slug}`}
-                                                            className="group/item text-lg flex items-center gap-2.5 px-3 py-1.5 rounded text-white hover:bg-warm hover:text-neutral-100 transition-colors duration-200"
-                                                        >
-                                                            <MoveUpRight
-                                                                className="w-4 shrink-0 transition-transform duration-200 group-hover/item:-translate-y-0.5 group-hover/item:translate-x-0.5 group-hover/item:text-white"
-                                                            />
-                                                            <span className="truncate">{submenu.title}</span>
-                                                        </Link>
-                                                    ))}
+                                                <Link href={items.path} className={`relative text-lg transition-all duration-200 ease-linear px-3.5 py-1.5 ${currentPath === items.path ? ' bg-primary text-white rounded-full' : ' text-zinc-800'} `}>
+                                                    {items.name}
+                                                </Link>
+                                            )}
+                                        {items.key === 'wet-wash-gloves' ? (
+                                            <div
+                                                className="absolute top-full left-1/2 -translate-x-1/2 mt-2 z-50 w-max bg-white rounded-xl shadow-md p-3 opacity-0 invisible translate-y-2 transition-all du    ration-200 group-hover/menu:opacity-100 group-hover/menu:visible group-hover/menu:translate-y-0 border border-gray-200"
+                                            >
+                                                {items.submenu?.map((submenu) => (
                                                     <Link
-                                                        href='/ultrasound-gel'
-                                                        className="group/item text-lg flex items-center gap-2.5 px-3 py-1.5 rounded text-white hover:bg-warm hover:text-neutral-100 transition-colors duration-200"
+                                                        key={submenu.key}
+                                                        href={submenu.path}
+                                                        className="group/item text-lg flex items-center gap-2.5 px-3 py-1.5 rounded text-zinc-900 hover:text-blue-600 transition-colors duration-200"
                                                     >
                                                         <MoveUpRight
-                                                            className="w-4 shrink-0 transition-transform duration-200 group-hover/item:-translate-y-0.5 group-hover/item:translate-x-0.5 group-hover/item:text-white"
+                                                            className="w-4 shrink-0 transition-transform duration-200 group-hover/item:-translate-y-0.5 group-hover/item:translate-x-0.5 group-hover/item:text-blue-600"
                                                         />
-                                                        <span className="truncate">
-                                                            Ultrasound Gel
-                                                        </span>
+                                                        <span className="truncate">{submenu.name}</span>
                                                     </Link>
-                                                </>
-                                            )}
-                                        </div>
-                                    ) : ''}
+                                                ))}
+                                            </div>
+                                        ) : items.key === 'products' ? (
+                                            <div
+                                                className="absolute top-full left-1/2 -translate-x-1/2 mt-2 z-50 w-max bg-white rounded-xl shadow-md p-3 opacity-0 invisible translate-y-2 transition-all duration-200 group-hover/menu:opacity-100 group-hover/menu:visible group-hover/menu:translate-y-0 border border-gray-200"
+                                            >
+                                                {isLoading ? (
+                                                    <span className="text-white px-3 py-1.5">Loading...</span>
+                                                ) : (
 
-                                </div>
-                            </Fragment>
-                        ))
-                    }
-                </div>
+                                                    <>
+                                                        {data?.map((submenu) => (
+                                                            <Link
+                                                                key={submenu.title}
+                                                                href={`/product/${submenu.slug}`}
+                                                                className="group/item text-lg flex items-center gap-2.5 px-3 py-1.5 rounded text-zinc-900 hover:text-blue-600 transition-colors duration-200"
+                                                            >
+                                                                <MoveUpRight
+                                                                    className="w-4 shrink-0 transition-transform duration-200 group-hover/item:-translate-y-0.5 group-hover/item:translate-x-0.5 group-hover/item:blue-600"
+                                                                />
+                                                                <span className="truncate">{submenu.title}</span>
+                                                            </Link>
+                                                        ))}
+                                                        <Link
+                                                            href='/ultrasound-gel'
+                                                            className="group/item text-lg flex items-center gap-2.5 px-3 py-1.5 rounded text-zinc-900 hover:text-blue-600 transition-colors duration-200"
+                                                        >
+                                                            <MoveUpRight
+                                                                className="w-4 shrink-0 transition-transform duration-200 group-hover/item:-translate-y-0.5 group-hover/item:translate-x-0.5 group-hover/item:text-blue-600"
+                                                            />
+                                                            <span className="truncate">
+                                                                Ultrasound Gel
+                                                            </span>
+                                                        </Link>
+                                                    </>
+                                                )}
+                                            </div>
+                                        ) : ''}
 
-                <button onClick={() => setIsMenuOpen((prev) => !prev)} className='lg:hidden w-12 h-12 flex items-center justify-center cursor-pointer bg-primary rounded-full '>
-                    <Menu className='text-white w-7 h-7' />
-                </button>
-            </nav>
+                                    </div>
+                                </Fragment>
+                            ))
+                        }
+                    </div>
 
-            <div className={`lg:hidden fixed inset-0 left-0 transition-transform duration-300 ease-linear bg-black/30 z-20 backdrop-blur-[4px] ${isMenuOpen ? 'translate-x-0' : ' -translate-x-full'} `} />
+                    <button onClick={() => setIsMenuOpen((prev) => !prev)} className='lg:hidden w-12 h-12 flex items-center justify-center cursor-pointer bg-primary rounded-full '>
+                        <Menu className='text-white w-7 h-7' />
+                    </button>
+                </nav>
+            </header>
 
-            <div className={`lg:hidden fixed z-40 inset-y-0 bg-primary max-w-[400px] w-full transition-transform duration-300 ease-linear p-5 overflow-hidden ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
+
+            <div className={`lg:hidden fixed inset-0 left-0 transition-transform duration-300 ease-linear bg-black/30 z-40 backdrop-blur-[4px] ${isMenuOpen ? 'translate-x-0' : ' -translate-x-full'} `} />
+
+            <div className={`lg:hidden fixed z-50 inset-y-0 bg-primary max-w-[400px] w-full transition-transform duration-300 ease-linear p-5 overflow-hidden ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
                 <div className='w-full h-full relative'>
                     <div className='relative w-full flex justify-between'>
                         <Link href={'/'}>
-                            <Image src={'/images/logo/logo.svg'} width={1000} height={600} alt='Ecoqual Healthcare Solutions' className='w-28 h-auto ' />
+                            <Image src={'/images/logo/logo.svg'} width={1000} height={600} alt='Ecoqual Healthcare Solutions' className='w-24 h-auto ' />
                         </Link>
                         <button className='w-10 h-10 bg-white rounded-full flex items-center justify-center border-none cursor-pointer' onClick={() => { setIsMenuOpen(false); setOpenSubMenu(null); }}>
                             <X />
@@ -253,13 +255,7 @@ export default function NavBar() {
                             menuItems.map(items => (
                                 <div className='relative flex flex-col' key={items.key}>
                                     {
-                                        items.key !== 'products' && items.key !== 'wet-wash-gloves' ? (
-                                            <Link href={items.path} className={`w-max relative text-xl transition-all font-medium duration-200 ease-linear px-4 py-1.5 rounded-full  ${currentPath === items.path ? 'bg-white text-primary' : 'text-white'} `}
-                                                onClick={() => { setIsMenuOpen(false) }}
-                                            >
-                                                {items.name}
-                                            </Link>
-                                        ) : (
+                                        items.submenu!?.length > 0 ? (
                                             <>
                                                 <button type="button"
                                                     onClick={() => toggleSubMenu(items.key)}
@@ -329,6 +325,12 @@ export default function NavBar() {
                                                     ) : ""
                                                 }
                                             </>
+                                        ) : (
+                                            <Link href={items.path} className={`w-max relative text-xl transition-all font-medium duration-200 ease-linear px-4 py-1.5 rounded-full  ${currentPath === items.path ? 'bg-white text-primary' : 'text-white'} `}
+                                                onClick={() => { setIsMenuOpen(false) }}
+                                            >
+                                                {items.name}
+                                            </Link>
                                         )
                                     }
                                 </div>
@@ -337,6 +339,6 @@ export default function NavBar() {
                     </div>
                 </div>
             </div>
-        </header>
+        </>
     )
 }
