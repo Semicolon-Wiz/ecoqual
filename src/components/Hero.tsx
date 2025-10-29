1f3d9b340bad7fa33b3474d4768b1c39842ec3d3
priyesshrai <priyeshrai369@gmail.com>
2025-10-29 16:18:22 +0530

    update home page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,102 +1,63 @@
 'use client';
-
-import React, { useRef } from 'react';
-import Link from 'next/link';
+import React, { useRef } from 'react'
 import {
     Carousel,
     CarouselContent,
     CarouselItem,
     CarouselNext,
     CarouselPrevious,
-    type CarouselApi,
-} from '@/components/ui/carousel';
-import Autoplay from 'embla-carousel-autoplay';
-import { Heading, Subheading } from '@/utils/Section';
-import { Button } from '@/components/ui/button';
+    type CarouselApi
+} from "@/components/ui/carousel"
+import Autoplay from "embla-carousel-autoplay";
 
-interface SlideData {
-    desktopImage: string;
-    mobileImage?: string;
+interface Data {
     title: string;
     summary: string;
-    buttonText: string;
-    buttonLink: string;
+    image: string;
 }
 
 export default function Hero() {
-    const slides: SlideData[] = [
-        {
-            desktopImage: '/images/hero/new-banner-01.png',
-            // mobileImage: '/images/hero/new-banner-01-mobile.png',
-            title: 'This is Heading One',
-            summary: 'This is a sub heading for slide one',
-            buttonText: 'Explore More',
-            buttonLink: '/category/facility-hygiene',
-        },
-        {
-            desktopImage: '/images/hero/new-banner-02.png',
-            // mobileImage: '/images/hero/new-banner-02-mobile.png',
-            title: 'This is Heading Two',
-            summary: 'This is a sub heading for slide two',
-            buttonText: 'Explore More',
-            buttonLink: '/category/patient-care',
-        },
-    ];
-
-    const [api, setApi] = React.useState<CarouselApi>();
-    const pluginRef = useRef(
-        Autoplay({ delay: 4000, stopOnInteraction: false, stopOnMouseEnter: true })
-    );
-
+    const banner = [
+        '/images/hero/banner-01.webp',
+        '/images/hero/banner-02.jpg',
+        '/images/hero/banner-03.jpg',
+    ]
+    const [api, setApi] = React.useState<CarouselApi>()
+    const plugin = useRef(Autoplay({ delay: 4000, stopOnInteraction: false, stopOnMouseEnter: true }))
     return (
-        <Carousel
-            className="w-full relative z-10"
-            plugins={[pluginRef.current]}
-            opts={{ loop: true }}
-            onMouseEnter={pluginRef.current.stop}
-            onMouseLeave={pluginRef.current.reset}
+        <Carousel className="w-full z-10"
+            plugins={[plugin.current]}
+            opts={{
+                loop: true,
+            }}
+            onMouseEnter={plugin.current.stop}
+            onMouseLeave={plugin.current.reset}
             setApi={setApi}
         >
             <CarouselContent>
-                {slides.map((slide, idx) => (
-                    <CarouselItem key={idx} className="relative w-full">
-                        <img
-                            src={slide.desktopImage}
-                            alt={slide.title}
-                            className="w-full h-[580px] object-cover object-top hidden md:block"
-                        />
-
-                        <img
-                            src={slide.mobileImage || slide.desktopImage}
-                            alt={slide.title}
-                            className="w-full h-auto object-cover object-top block md:hidden"
-                        />
-
-                        <div className="md:absolute md:top-1/2 md:left-1/12 md:-translate-y-1/2 relative p-5 md:p-0 text-white">
-                            <Heading>{slide.title}</Heading>
-                            <Subheading classname="text-left">{slide.summary}</Subheading>
-
-                            <Link href={slide.buttonLink}>
-                                <Button className="mt-4">{slide.buttonText}</Button>
-                            </Link>
-                        </div>
-                    </CarouselItem>
-                ))}
+                {
+                    banner.map((data, key) => (
+                        <CarouselItem key={key}>
+                            <img src={data} alt='Bannet' className='w-full h-auto' />
+                        </CarouselItem>
+                    ))
+                }
             </CarouselContent>
             <CarouselPrevious
                 onClick={() => {
                     api?.scrollPrev();
-                    pluginRef.current.reset();
+                    plugin.current?.reset();
                 }}
-                className="border-none w-11 h-11 bg-zinc-900 text-white left-10 text-3xl cursor-pointer hover:bg-zinc-800 absolute top-1/2 -translate-y-1/2"
+                className='border-none w-11 h-11 bg-zinc-900 text-white left-10 text-3xl cursor-pointer hover:bg-zinc-800'
             />
             <CarouselNext
                 onClick={() => {
                     api?.scrollNext();
-                    pluginRef.current.reset();
+                    plugin.current?.reset();
                 }}
-                className="border-none w-11 h-11 bg-zinc-900 text-white right-10 text-3xl cursor-pointer hover:bg-zinc-800 absolute top-1/2 -translate-y-1/2"
+                className='border-none w-11 h-11 bg-zinc-900 text-white text-3xl right-10 cursor-pointer hover:bg-zinc-800'
             />
         </Carousel>
-    );
+
+    )
 }
