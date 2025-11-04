fda6061f2714e35a0931c35ba001293537b7a875
priyesshrai <priyeshrai369@gmail.com>
2025-11-04 16:47:05 +0530

    update product ui

--- src/app/[categoryId]/[subCategory]/ProductList.tsx
+++ src/app/[categoryId]/[subCategory]/ProductList.tsx
@@ -67,7 +67,7 @@ export default function ProductList({ categoryId, subCategory }: { categoryId: s
                             <div className="h-6 bg-gray-200 rounded-md w-2/3 mx-auto" />
                         </div>
 
-                        <div className='w-full relative grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-3 gap-y-10'>
+                        <div className='w-full relative grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-3 md:gap-y-10 gap-y-5'>
                             {
                                 Array.from({ length: 6 }).map((_, id) => (
                                     <ProductSkeleton key={id} />
@@ -99,7 +99,7 @@ export default function ProductList({ categoryId, subCategory }: { categoryId: s
             <Wrapper>
                 <div className='relative w-full flex flex-col gap-8'>
                     <div className='flex-1 text-center'>
-                        <Heading classname='text-secondry'>
+                        <Heading classname='text-secondry sm:text-center text-left'>
                             {data?.subcategory.title ?? ''}
                         </Heading>
                         <Subheading classname='max-w-4xl mx-auto !font-normal !text-left !text-gray-700'>
@@ -110,7 +110,7 @@ export default function ProductList({ categoryId, subCategory }: { categoryId: s
                     {
                         data?.products && data.products.length > 0 ? (
                             <motion.div
-                                className='w-full relative grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-3 gap-y-10'
+                                className='w-full relative grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-3 md:gap-y-10 gap-y-5'
                                 variants={cardContainerVariants}
                                 initial="hidden"
                                 whileInView="visible"
@@ -120,32 +120,32 @@ export default function ProductList({ categoryId, subCategory }: { categoryId: s
                                     data?.products.map((items, idx) => (
                                         <motion.div
                                             key={idx}
-                                            className='relative w-full h-full bg-white shadow-md group rounded-md'
+                                            className='relative w-full h-full bg-white shadow-md group rounded-md overflow-hidden'
                                             variants={cardVarient}
 
                                         >
-                                            <Link href={`/${categoryId}/${subCategory}/${items.slug}`} className='block relative w-full h-[300px]'>
+                                            <Link href={`/${categoryId}/${subCategory}/${items.slug}`} className='block relative w-full h-[250px] '>
                                                 <Image src={items.image} alt={items.title} width={1920} height={1080}
-                                                    className='w-full h-full object-contain object-center'
+                                                    className='w-full h-full object-center object-cover'
                                                 />
                                             </Link>
-                                            <div className='relative w-full p-3 pb-6 z-10'>
-                                                <span className='text-sm font-medium text-blue-600'>
+                                            <div className='relative w-full p-3 z-10'>
+                                                <span className='text-xs font-medium text-blue-600'>
                                                     {data.category.title}
                                                 </span>
-                                                <Link href={`/${categoryId}/${subCategory}/${items.slug}`} className='block mt-1 text-gray-800 !font-montserrat md:text-lg text-base font-semibold '>
+                                                <Link href={`/${categoryId}/${subCategory}/${items.slug}`} className='block mt-1 text-gray-800 !font-montserrat md:text-lg text-sm font-semibold '>
                                                     {items.title}
                                                 </Link>
 
                                                 <div
-                                                    className="prose mt-2 max-w-none text-gray-700 line-clamp-3 text-sm"
+                                                    className="hidden md:block prose mt-2 max-w-none text-gray-700 line-clamp-3 text-xs dis"
                                                     dangerouslySetInnerHTML={{ __html: items.product_description }}
                                                 />
-
-                                                <Link href={`/${categoryId}/${subCategory}/${items.slug}`} className='ml-auto mt-6  w-max text-base flex items-center gap-1 justify-center text-blue-700 underline underline-offset-4 '>
+{/* 
+                                                <Link href={`/${categoryId}/${subCategory}/${items.slug}`} className='ml-auto mt-6  w-max md:text-base text-xs flex items-center gap-1 justify-center text-blue-700 underline underline-offset-4 '>
                                                     View Product
                                                     <MoveUpRight size={14} />
-                                                </Link>
+                                                </Link> */}
                                             </div>
                                         </motion.div>
                                     ))
